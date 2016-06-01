d641031057792763e8568829289cbf13a2952b26
Phillip Schmidt <daPhoosa@users.noreply.github.com>
2016-06-01 16:52:24 -0500

    Complete sorting re-write
    Data is no longer sorted.  It is stored in a ring buffer and maps are used to determine the location of the median sample.  With this change, filtering is faster.  In the future, adapting the filter to use other data types (longs or floats) should have minimal effect on performance.

--- MedianFilter.cpp
+++ MedianFilter.cpp
@@ -1,162 +1,180 @@
 /*
-  MedianFilter.cpp - Median Filter for the Arduino platform.
-  Copyright (c) 2013 Phillip Schmidt.  All right reserved.
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	MedianFilter.cpp - Median Filter for the Arduino platform.
+	Copyright (c) 2013 Phillip Schmidt.  All right reserved.
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 /* 
- 
  	A median filter object is created by by passing the desired filter window size on object creation.
 	The window size should be an odd number between 3 and 255.  
   
-	New data is added to the median filter by passing the data through the write() method.
+	New data is added to the median filter by passing the data through the in() function.  The new medial value is returned.
 	The new data will over-write the oldest data point, then be shifted in the array to place it in the correct location. 
   
-	The current median value is returned by the read() method.
+	The current median value is returned by the out() function for situations where the result is desired without passing in new data.
  
 	!!! All data must be type INT.  !!!
- 
+	
+	Window Size / avg processing time [us]
+	5  / 22
+	7  / 30
+	9  / 40
+	11 / 49
+	21 / 99
+	
 */
 
 #include "MedianFilter.h"
 
 	
 
-MedianFilter::MedianFilter(byte size, int seed)
+MedianFilter::MedianFilter(const byte size, const int seed)
 {
 
-	if(size < 3){size = 3;}  //  prevent undersized windows
-	
-	medFilterWin = size;				// number of samples in sliding median filter window - usually odd #
-	medDataPointer = size >> 1;			// mid point of window
-	sortedData = (int*) calloc (size, sizeof(int));		// array for data sorted by size
-	historyMap = (byte*) calloc (size, sizeof(byte));	// array for locations of data in sorted list (arranged in looped age order)
+	medFilterWin = max(size, 3);			// number of samples in sliding median filter window - usually odd #
+	medDataPointer = size >> 1;				// mid point of window
+	data = (int*) calloc (size, sizeof(int));			// array for data
+	sizeMap = (byte*) calloc (size, sizeof(byte));		// array for locations of data in sorted list
 	locationMap = (byte*) calloc (size, sizeof(byte));	// array for locations of history data in map list
-	ODP = 0;							// oldest data point location in historyMap
+	oldestDataPoint = medDataPointer;					// oldest data point location in data array
 	
-	for(byte i=0; i<medFilterWin; i++) // initialize the arrays
+	for(byte i = 0; i < medFilterWin; i++) // initialize the arrays
 	{
-		historyMap[i] = i;				// start map with straight run
-		locationMap[i] = i;				// start map with straight run
-		sortedData[i] = seed;			// populate with seed value
+		sizeMap[i] = i;			// start map with straight run
+		locationMap[i] = i;		// start map with straight run
+		data[i] = seed;			// populate with seed value
 	}
 	
 }
 
 int MedianFilter::in(int value)
 {  
+	// sort sizeMap
+	// small vaues on the left (-)
+	// larger values on the right (+)
 
 	boolean dataMoved = false;
-	byte rightEdge = medFilterWin - 1;
+	const byte rightEdge = medFilterWin - 1;  // adjusted for zero indexed array
 	
-	sortedData[historyMap[ODP]] = value;  // store new data in location of oldest data
+	data[oldestDataPoint] = value;  // store new data in location of oldest data in ring buffer
 	
-	if(historyMap[ODP] != 0) // don't check left neighbours if at the extreme left
-	{
-		for(int i = historyMap[ODP]; i > 0; i--)	//index through left adjacent data
-		{
-			int j = i - 1;	// neighbour location
-			if(sortedData[i] < sortedData[j])
-			{
-				//Serial.print("<");
-				tempData = sortedData[j];		// store neighbour data in temp
-				tempMap = locationMap[j];		// store position of adjacent data in historyMap
+	// SORT LEFT (-) <======(n) (+)
+	if(locationMap[oldestDataPoint] > 0){ // don't check left neighbours if at the extreme left
+		
+		for(byte i = locationMap[oldestDataPoint]; i > 0; i--){	//index through left adjacent data
 			
-				sortedData[j] = sortedData[i];	// move new data to neighbour location
-				historyMap[ODP] = j;
-				locationMap[j] = ODP;
+			byte n = i - 1;	// neighbour location
+			
+			if(data[oldestDataPoint] < data[sizeMap[n]]){ // find insertion point, move old data into position
+
+				sizeMap[i] = sizeMap[n];	// move existing data right so the new data can go left
+				locationMap[sizeMap[n]]++;
 				
-				sortedData[i] = tempData;		// swap neighbour data back in
-				historyMap[tempMap] = i;
-				locationMap[i] = tempMap;
+				sizeMap[n] = oldestDataPoint; // assign new data to neighbor position
+				locationMap[oldestDataPoint]--;
 				
-				dataMoved = true; 
+				dataMoved = true;
+				
 			}
-			else
-			{
-				i=0; // abort loop if left neighbour is larger (faster than "break;")
+			else {
+
+				break; // stop checking once a smaller value is found on the left 
+				
 			}
 		}
-	} // end shift data to left
+	}
 	
-	if(historyMap[ODP] != rightEdge && dataMoved == false) // don't check right neighbours if at the extreme right or data already moved
-	{
-		for(int i = historyMap[ODP]; i < rightEdge; i++) //index through right adjacent data
-		{
-			int j = i + 1;	// neighbour location
-			if(sortedData[i] > sortedData[j])
-			{
-				//Serial.print(">");
-				tempData = sortedData[j]; // store neighbour data in temp
-				tempMap = locationMap[j];	// store position of adjacent data in historyMap
+	// SORT RIGHT (-) (n)======> (+)
+	if(!dataMoved && locationMap[oldestDataPoint] < rightEdge){ // don't check right if at right border, or the data has already moved
+
+		for(int i = locationMap[oldestDataPoint]; i < rightEdge; i++){	//index through left adjacent data
+
+			int n = i + 1;	// neighbour location
 			
-				sortedData[j] = sortedData[i];// move new data to neighbour location
-				historyMap[ODP] = j;
-				locationMap[j] = ODP;
+			if(data[oldestDataPoint] > data[sizeMap[n]]){ // find insertion point, move old data into position
+
+				sizeMap[i] = sizeMap[n];	// move existing data left so the new data can go right
+				locationMap[sizeMap[n]]--;
 				
-				sortedData[i] = tempData; // swap neighbour data back in
-				historyMap[tempMap] = i;
-				locationMap[i] = tempMap;
+				sizeMap[n] = oldestDataPoint; // assign new data to neighbor position
+				locationMap[oldestDataPoint]++;
+
 			}
-			else
-			{
-				i=medFilterWin; // abort loop if right neighbour is smaller (faster than "break;")
-			}
+			else {
+
+				break; // stop checking once a smaller value is found on the right 
+				
+			}	
 		}
-	} // end shift data to right
+	}	
 	
-	ODP++; 		// increment and wrap
-	if(ODP >= medFilterWin) ODP = 0;
+	oldestDataPoint++; 		// increment and wrap
+	if(oldestDataPoint == medFilterWin) 
+		oldestDataPoint = 0;
 	
-	return sortedData[medDataPointer];
+	return data[sizeMap[medDataPointer]];
 }
 
 
 int MedianFilter::out() // return the value of the median data sample
 {
-	return  sortedData[medDataPointer];     
+	return  data[sizeMap[medDataPointer]];     
 }
 
+
+
+// *** debug fuctions ***
 /*
 void MedianFilter::printData() // display sorting data for debugging
 {
 	for(int i=0; i<medFilterWin; i++)
-	{
-		Serial.print(sortedData[i]);
+	{	
+		Serial.print(data[i]);
 		Serial.print("\t");
 	}
-	Serial.print("\t");
-	Serial.println("Data sorted by size");
-	
+	Serial.println("Data in ring buffer");
+}
+
+void MedianFilter::printSizeMap()
+{ 
 	for(int i=0; i<medFilterWin; i++)
 	{
-		Serial.print(historyMap[i]);
+		Serial.print(sizeMap[i]);
 		Serial.print("\t");
 	}
-	Serial.print("\t");
-	Serial.println("Locations of data sorted by age");
-	
+	Serial.println("Size Map, data sorted by size");
+}
+
+void MedianFilter::printLocationMap()
+{	
 	for(int i=0; i<medFilterWin; i++)
 	{
 		Serial.print(locationMap[i]);
 		Serial.print("\t");
 	}
-	Serial.print("\t");
-	Serial.println("Location of age data in age list sorted by data size");
-	Serial.println("");
+	Serial.println("Location Map, size data sorted by age");
 }  
-*/
 
+void MedianFilter::printSortedData() // display data for debugging
+{
+	for(int i=0; i<medFilterWin; i++)
+	{
+		Serial.print(data[sizeMap[i]]);
+		Serial.print("\t");
+	}
+	Serial.println("Data sorted by size");
+}
+*/
--- MedianFilter.h
+++ MedianFilter.h
@@ -21,10 +21,10 @@
 	A median filter object is created by by passing the desired filter window size on object creation.
 	The window size should be an odd number between 3 and 255.  
   
-	New data is added to the median filter by passing the data through the write() method.
+	New data is added to the median filter by passing the data through the in() function.  The new medial value is returned.
 	The new data will over-write the oldest data point, then be shifted in the array to place it in the correct location. 
   
-	The current median value is returned by the read() method.
+	The current median value is returned by the out() function for situations where the result is desired without passing in new data.
  
 	!!! All data must be type INT.  !!!
  */
@@ -41,17 +41,19 @@
 			MedianFilter(byte size, int seed);
 			int in(int value);
 			int out(); 
+			
 			//void printData();		// used for debugging
+			//void printSizeMap();
+			//void printLocationMap();
+			//void printSortedData();
 			
 		private:
 			byte medFilterWin;		// number of samples in sliding median filter window - usually odd #
 			byte medDataPointer;	// mid point of window
-			int * sortedData;		// array pointer for data sorted by size
-			byte * historyMap;		// array pointer for locations of history data in sorted list
+			int  * data;			// array pointer for data sorted by age in ring buffer
+			byte * sizeMap;			// array pointer for locations data in sorted by size
 			byte * locationMap;		// array pointer for data locations in history map
-			int ODP;				// oldest data point in accelRawHistory
-			int tempData;			// temp data storage while swapping data locations
-			byte tempMap;			// temp map storage while swapping data locations
+			byte oldestDataPoint;	// oldest data point location in ring buffer
 	};
 
 #endif
